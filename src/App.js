718ff8497527d3558142e2989df64a1e83c550b5
Ankan <adas8682@gmail.com>
2021-10-07 22:37:56 +0530

    floating avatar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import './particle.css';
 import Profile from './Profile';
-import Links from  './Links';
+import Links from './Links';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LanguageIcon from '@material-ui/icons/Language';
@@ -11,28 +12,32 @@ import YouTubeIcon from '@material-ui/icons/YouTube';
 
 
 
+
 function App() {
   return (
-    <div className="App">
-    
-
-     {/* Avatar component  */}
-      <Profile />
-     
-     {/* Links component  passing the link number as a props  */}
-      <Links title=' Join Me on Facebook'      icon={<FacebookIcon />}          url='https://www.facebook.com/AnkanDance'  />
-      <Links title='Follow Me on Instagram'    icon={<InstagramIcon />}             url='https://www.instagram.com/ankaninperson/' />
-      <Links title='Portfolio'                icon={<LanguageIcon/>}                 url='https://ankandas.netlify.app/' />
-      <Links title='Twitter'               icon={<TwitterIcon />}                 url='https://twitter.com/ankan0008?s=08' />
-      <Links title='Medium'                icon={<BookIcon />}                  url='https://ankandasx.medium.com/' />
-      <Links title='Github'                 icon={<GitHubIcon />}                  url='https://github.com/Ankan982' />
-      <Links title='Subscribe'              icon={<YouTubeIcon />}                   url='https://www.youtube.com/c/ankandas' />
+    <div className="App1">
       
 
-     
-       {/* Footer component  */}
-    
   
+
+      {/* Avatar component  */}
+      <Profile />
+
+      {/* Links component  passing the link number as a props  */}
+      <Links title='Instagram' icon={<InstagramIcon />} url='https://www.instagram.com/ankaninperson/' />
+      <Links title='Portfolio' icon={<LanguageIcon />} url='https://ankandas.netlify.app/' />
+      <Links title=' Join Me on Facebook' icon={<FacebookIcon />} url='https://www.facebook.com/AnkanDance' />
+      <Links title='Twitter' icon={<TwitterIcon />} url='https://twitter.com/ankan0008?s=08' />
+      <Links title='Medium' icon={<BookIcon />} url='https://ankandasx.medium.com/' />
+      <Links title='Github' icon={<GitHubIcon />} url='https://github.com/Ankan982' />
+      <Links title='Subscribe' icon={<YouTubeIcon />} url='https://www.youtube.com/c/ankandas' />
+
+
+
+      {/* Footer component  */}
+      <p className="footer">Created by Ankan Das using ReactJs</p>
+
+
     </div>
   );
 }
